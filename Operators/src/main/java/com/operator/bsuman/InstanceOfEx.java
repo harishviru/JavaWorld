8fcc85bd591e00a6b682d402f0c668e6a8345ae4
Bsuman7015 <101004327+Bsuman7015@users.noreply.github.com>
2022-12-18 19:40:50 +0530

    Arthimatic operators

---
+++ Operators/src/main/java/com/operator/bsuman/InstanceOfEx.java
@@ -0,0 +1,10 @@
+package com.operator.bsuman;
+
+public class InstanceOfEx {
+
+	public static void main(String[] args) {
+	
+		String s= new String("suman");
+		System.out.println(s instanceof Object);
+	}
+}
