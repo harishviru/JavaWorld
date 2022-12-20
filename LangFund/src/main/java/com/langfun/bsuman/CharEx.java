8f50ffd3540c4c385b8b9238328937c2df4962e3
Bsuman7015 <101004327+Bsuman7015@users.noreply.github.com>
2022-12-20 19:58:53 +0530

    changes in lang Fundamentals

---
+++ LangFund/src/main/java/com/langfun/bsuman/CharEx.java
@@ -0,0 +1,12 @@
+package com.langfun.bsuman;
+
+public class CharEx {
+ public static void main(String[] args) {
+	char[] ch= {'a','e','i','o','u'};
+	String[] s= {"hari","saleem","laxman"};
+	System.out.println(ch);
+	System.out.println(s);
+	
+	
+}
+}
---
+++ LangFund/src/main/java/com/langfun/bsuman/Pvariables.java
@@ -0,0 +1,16 @@
+package com.langfun.bsuman;
+
+public class Pvariables {
+	int i=10;
+	
+	public static void main(String[] args) {
+		//System.out.println(i);
+		
+		Pvariables p= new Pvariables();
+		System.out.println(p.i);
+		
+	}
+	
+	
+
+}
